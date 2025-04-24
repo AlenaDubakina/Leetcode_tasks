ee52b9f48aa29b617a2b3fcb51b5f3486a8bbd4f
Alena Dubakina <alenadubakina@gmail.com>
2025-04-24 14:17:56 +0500

    RemoveDuplicates_task

---
+++ src/main/java/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+public class RemoveDuplicates {
+    /* Дан целочисленный массив nums, отсортированный в неубывающем порядке ,
+    удалить дубликаты на месте так, чтобы каждый уникальный элемент появлялся только один раз .
+    Относительный порядок элементов должен оставаться прежним .
+    Затем вернуть количество уникальных элементов вnums .
+    */
+
+    public static int removeDuplicates(int[] nums) {
+        int[] uniqueArr = new int[nums.length];
+        uniqueArr[0] = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != uniqueArr[count - 1]) {
+                uniqueArr[count] = nums[i];
+                count++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = uniqueArr[i];
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2};
+        System.out.println(removeDuplicates(arr));
+    }
+}
