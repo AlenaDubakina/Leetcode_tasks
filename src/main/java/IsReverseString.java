226d6e4e29b09e8a518a8be1a5c9b641ac97658e
Alena Dubakina <alenadubakina@gmail.com>
2025-10-20 10:59:57 +0500

    IsReverseString_task

---
+++ src/main/java/IsReverseString.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class IsReverseString {
+    /*
+    Напишите функцию, которая переворачивает строку.
+    Входная строка представлена массивом символов s.
+    Это необходимо сделать, изменив входной массив
+    на месте с использованием O(1)дополнительной памяти.
+    */
+
+    public static void reverseString(char[] s) {
+        char temp;
+        for (int i = 0; i < s.length / 2; i++) {
+            temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        reverseString(new char[]{'h', 'e', 'l', 'l', 'o'});
+        reverseString(new char[]{'h', 'e', 'l', 'l', 'o', '!'});
+    }
+}
